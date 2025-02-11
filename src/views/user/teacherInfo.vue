1d99f8b1799363efefdc29f5d336c861c5a9f6bd
catyugu <1435221207@qq.com>
2025-02-11 13:57:05 +0800

    Commit

--- src/views/user/teacherInfo.vue
+++ src/views/user/teacherInfo.vue
@@ -1,29 +1,38 @@
-
 <template>
+  <div>
 
+  </div>
 </template>
 <script>
 
-export default({
-  props:{
-    teacherID:{
-      type:String,
-      default:''
+export default ({
+  props: {
+    teacherID: {
+      type: String,
+      default: ''
     }
   },
   data() {
     return {
-      teacherInfo:{
+      teacherInfo: {
         avatar: '',
         name: '',
         gender: '',
-
+        introduction: '',
+        email: '',
+        phone: ''
       }
-    }
+    };
   },
   methods: {
+    getTeacherInfoByID() {
+      this.$store.dispatch('getTeacherInfoByID', this.teacherID)
+        .then(res => {
+          this.teacherInfo = res.data.data;
+        });
+    }
   }
-})
+});
 </script>
 
 <style scoped lang="scss">
