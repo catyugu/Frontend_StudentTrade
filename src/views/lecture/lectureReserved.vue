19121c202002bcd6920e1f3413112d3bf3773793
catyugu <1435221207@qq.com>
2025-02-13 14:13:42 +0800

    Commit

---
+++ src/views/lecture/lectureReserved.vue
@@ -0,0 +1,33 @@
+
+
+<template>
+<div>
+ <lecture-display-list :lectureList="lectureList" />
+</div>
+</template>
+<script>
+import LectureDisplayList from '@/components/lectureDisplayList.vue';
+
+export default {
+  components: { LectureDisplayList },
+  methods: {
+  },
+  props: {
+    userID: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      lectureList: [],
+    }
+  },
+  created() {
+    this.lectureList = this.$store.state.userInfo.lectureReserveList;
+  },
+}
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/project/projectLike.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <project-display-list :projectList="projectIDList" />
+  </div>
+</template>
+<script>
+import ProjectDisplayList from '@/components/projectDisplayList.vue';
+
+export default {
+  components: { ProjectDisplayList },
+  data() {/***/},
+  created() {
+    this.userID = this.$route.params.userID;
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
